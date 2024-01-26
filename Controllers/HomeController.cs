e752a6af7bc5bd2f4bdd915471a8ceeb4acee046
siphamandlamzobe <corneliusmzobe@gmail.com>
2024-01-26 09:50:42 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace k8Test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetResponse()
+        {
+            return Ok(new { message = "live..." });
+        }
+    }
+}
